4a6b5913bd3951d8ba4ed6dae37741819f40d149
Anastasiia Frolova <lettertofrolova@gmail.com>
2025-04-16 11:11:35 +0300

    initialize repo; add architectural structure of framework

---
+++ configuration.py
@@ -0,0 +1,13 @@
+import os
+
+MOBILE_DEVICES = {
+    'samsung': 'Galaxy S9+',
+    'default': 'Nexus 7',
+}
+
+TWITCH_MOBILE_URL = "https://m.twitch.tv"
+
+LANGUAGE = 'en'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+SCREENSHOTS_DIR = os.path.join(PROJECT_ROOT, "screenshots")
