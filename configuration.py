25a179703856cfa370ec2cd0ba7343520e16507a
Anastasiia Frolova <lettertofrolova@gmail.com>
2025-04-16 15:41:20 +0300

    add final types

--- configuration.py
+++ configuration.py
@@ -1,13 +1,13 @@
 import os
+from typing import Final
 
 MOBILE_DEVICES = {
     'samsung': 'Galaxy S9+',
     'default': 'Nexus 7',
 }
 
-TWITCH_MOBILE_URL = "https://m.twitch.tv"
-
+TWITCH_MOBILE_URL: Final[str] = "https://m.twitch.tv"
 LANGUAGE = 'en'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-SCREENSHOTS_DIR = os.path.join(PROJECT_ROOT, "screenshots")
+PROJECT_ROOT: Final[str] = os.path.dirname(os.path.abspath(__file__))
+SCREENSHOTS_DIR: Final[str] = os.path.join(PROJECT_ROOT, "screenshots")
